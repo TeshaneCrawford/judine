dc677dfe8387a11adfc415240156f72205f6a9fa
Teshane Crawford <crawfordteshane@gmail.com>
2024-03-16 22:54:20 -0500

    update: :arrow_up: Add text-gradient and judine-prose utility classes, and update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -44,7 +44,43 @@ module.exports = {
     },
   },
   plugins: [
-    plugin()
+    plugin(
+      function ({ matchUtilities, theme }) {
+        matchUtilities(
+          {
+            "content-visibility": (value) => {
+              return {
+                "content-visibility": value,
+              };
+            },
+          },
+          {
+            values: theme("contentVisibility"),
+          }
+        );
+        matchUtilities(
+          {
+            "contain-intrinsic-size": (value) => {
+              return {
+                "contain-intrinsic-size": value,
+              };
+            },
+          },
+          {
+            values: theme("containIntrinsicSize"),
+          }
+        );
+      },
+      {
+        theme: {
+          contentVisibility: {
+            auto: "auto",
+            hidden: "hidden",
+            visible: "visible",
+          },
+        },
+      }
+    )
   ],
 }
 
