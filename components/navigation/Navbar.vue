292941b155291ca8ad4a66ebd1c7465f1341eed3
Teshane Crawford <crawfordteshane@gmail.com>
2024-03-19 22:11:31 -0500

    Feat: :sparkles: Add components
    Add default layout, Navbar, Footer, and Container components

---
+++ components/navigation/Navbar.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+// import { onBeforeUnmount, onMounted, ref } from 'vue';
+
+const links = [
+  { name: 'Home', to: '/' },
+  { name: 'Gallery', to: '/gallery'},
+  { name: 'Art', to: '/art'},
+  { name: 'Stories', to: '/stories'},
+  { name: 'Bio', to: '/bio'},
+  { name: 'Contact', to: '/contact'},
+] as { name: string; to: string }[];
+
+onMounted(() => {
+  window.addEventListener('scroll', onScroll)
+});
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', onScroll)
+});
+
+const showHeader = ref(true);
+const lastScrollPosition = ref(0);
+
+const onScroll = () => {
+  const currentScrollPosition =
+    window.scrollY || document.documentElement.scrollTop;
+  // Momentum scrolling on iOS can cause the scroll position to be negative
+  if (currentScrollPosition < 0) return;
+
+  // add 60px delay
+  if (Math.abs(currentScrollPosition - lastScrollPosition.value) < 60) return;
+
+  // show if scrolling up
+  showHeader.value = currentScrollPosition < lastScrollPosition.value;
+
+  lastScrollPosition.value = currentScrollPosition;
+};
+</script>
+
+<template>
+  <div>
+    Component: navigation/Navbar
+  </div>
+</template>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup></script>
+
+<template>
+  <article class="min-h-dvh flex flex-col items-stretch">
+    <!-- Ring -->
+    <div class="fixed inset-0 flex justify-center sm:px-8">
+      <div class="flex w-full max-w-7xl lg:px-8">
+        <div
+          class="w-full bg-white ring-1 ring-zinc-200/50 dark:bg-zinc-900 dark:ring-zinc-500/20"
+        />
+      </div>
+    </div>
+
+    <!-- Background -->
+    <Container class="relative">
+      <div class="absolute top-0 right-0">
+        <NuxtImg
+          src=""
+          aria-hidden="true"
+          class="w-[44rem]"
+          format="webp"
+          height="944"
+          width="586"
+        />
+      </div>
+    </Container>
+
+    <!-- Navbar -->
+    <Navbar />
+
+    <!-- Main -->
+    <main class="judine-prose">
+      <Container>
+        <slot />
+      </Container>
+    </main>
+
+    <!-- Footer -->
+    <Footer class="mt-auto" />
+  </article>
+</template>
+
+<style scoped></style>
