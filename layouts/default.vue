7930fbc7e8765bd17786b131c477029770acee8e
Teshane Crawford <crawfordteshane@gmail.com>
2024-03-20 20:07:52 -0500

    Feat: :sparkles: Update page title and layout
    Fix parallax script that was preventing images from showing

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup></script>
 
 <template>
-  <article class="min-h-screen flex flex-col items-stretch">
+  <article class="min-h-dvh flex flex-col items-stretch">
     <!-- ring -->
-    <div class="fixed inset-0 flex justify-center sm:px-8">
+    <!-- <div class="fixed inset-0 flex justify-center sm:px-8">
       <div class="flex w-full max-w-7xl lg:px-8">
         <div
           class="w-full bg-white ring-1 ring-zinc-200/50 dark:bg-zinc-900 dark:ring-zinc-500/20"
         />
       </div>
-    </div>
+    </div> -->
 
     <!-- background -->
     <Container class="relative">
