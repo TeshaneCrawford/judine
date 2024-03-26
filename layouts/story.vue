34a9a6dd7fe4d54a9874d19de31161056846920f
Teshane Crawford <crawfordteshane@gmail.com>
2024-03-25 20:25:40 -0500

    Update: Add new story layout and triple images component

---
+++ layouts/story.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <!-- Background -->
+    <Container class="relative">
+      <div class="absolute top-0 right-0">
+        <NuxtImg
+          src="img/bg-glow.png"
+          aria-hidden="true"
+          class="w-[44rem]"
+          format="webp"
+          width="944"
+          height="586"
+        />
+      </div>
+    </Container>
+
+    <div class="relative">
+      <NavigationNavbar />
+      <main>
+        <Container>
+          <div class="mt-12 lg:mt-24 judine-prose">
+            <slot />
+          </div>
+        </Container>
+      </main>
+      <NavigationFooter class="mt-auto" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
