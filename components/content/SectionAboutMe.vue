fc73f3515a279f7927a26cfc70b235f5551eae5f
Teshane Crawford <crawfordteshane@gmail.com>
2024-03-26 00:00:24 -0500

    Refactor story layout and about me section

--- components/content/SectionAboutMe.vue
+++ components/content/SectionAboutMe.vue
@@ -20,20 +20,28 @@ defineProps({
     <div class="grid grid-cols-1 lg:grid-cols-7 gap-8">
       <div class="col-span-4 flex flex-col gap-8 order-2 lg:order-1">
         <h2 class="text-4xl lg:text-6xl font-display text-zinc-800 dark:text-zinc-200">
-          {{ firstname }} <br />
+          {{ firstname }} <br>
           {{ lastname }}
         </h2>
         <div class="dark:text-zinc-400 flex flex-col gap-4">
-          <slot name="description"></slot>
+          <slot name="description" />
         </div>
-        <NuxtImg src="/img/home/sign.png" alt="signature" class="h-8 mr-auto" />
+        <NuxtImg
+          src="/img/home/sign.png"
+          alt="signature"
+          class="h-8 mr-auto"
+        />
       </div>
 
-      <div v-parallax
+      <div
+        v-parallax
         data-rellax-xs-speed="0"
         data-rellax-mobile-speed="0"
         data-rellax-tablet-speed="0"
-        data-parallax-speed="-1" data-rellax-percentage="0.5" class="col-span-3 order-1 lg:order-2 pl-2 pt-2">
+        data-parallax-speed="-1"
+        data-rellax-percentage="0.5"
+        class="col-span-3 order-1 lg:order-2 pl-2 pt-2"
+      >
         <div class="lg:absolute aspect-square lg:aspect-[2/3] flex-none overflow-hidden bg-zinc-100 dark:bg-zinc-800 w-32 lg:w-72 rounded-2xl rotate-3">
           <NuxtImg
             placeholder
@@ -50,7 +58,7 @@ defineProps({
       </div>
     </div>
 
-    <hr class="my-10 h-px border-0 bg-zinc-200 dark:bg-zinc-800" />
-    <slot name="extra"></slot>
+    <hr class="my-10 h-px border-0 bg-zinc-200 dark:bg-zinc-800">
+    <slot name="extra" />
   </div>
 </template>
---
+++ components/content/StoriesGalleryHeader.vue
@@ -0,0 +1,78 @@
+<script lang="ts" setup>
+defineProps({
+  date: {
+    type: String,
+    default: '',
+  },
+  location: {
+    type: String,
+    default: '',
+  },
+  camera: {
+    type: String,
+    default: '',
+  },
+  film: {
+    type: String,
+    default: '',
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <section class="byline">
+    <NuxtLink class="project-icons">
+      <li>
+        <Icon name="i-ri-calendar-line" />
+        {{ date }}
+      </li>
+      <li>
+        <Icon name="i-ri-map-pin-2-line" />
+        {{ location }}
+      </li>
+      <li>
+        <Icon name="i-ri-camera-line" />
+        {{ camera }}
+      </li>
+      <li>
+        <Icon name="i-ri-film-line" />
+        {{ film }}
+      </li>
+    </NuxtLink>
+  </section>
+  <div>
+    <blockquote>
+      {{ description }}
+    </blockquote>
+  </div>
+</template>
+
+<style scoped>
+.project-icons li {
+  font-size: 15px;
+  display: inline-block;
+  padding: 0 15px;
+  border-right: 1px solid #999;
+}
+
+.project-icons li:last-child {
+  border-right: 0;
+}
+
+.byline {
+  display: flex;
+  justify-content: center;
+  /* margin: 20px 0; */
+}
+
+blockquote {
+  color: #a5a4a4;
+  font-style: italic;
+  text-align: center;
+  margin-bottom: 30px;
+}
+</style>
