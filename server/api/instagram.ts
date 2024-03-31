fcd913720c119e510a0a5dc1ac08b12208f07812
Teshane Crawford <crawfordteshane@gmail.com>
2024-03-30 23:09:29 -0500

    Update Instagram access token

--- server/api/instagram.ts
+++ server/api/instagram.ts
@@ -5,7 +5,7 @@ interface InstagramResponse {
 }
 
 export default defineEventHandler(async (event) => {
-  const accessToken = 'IGQWRORjN3dTBRSnFMUThjaHp6WEhkbzlQdl95dkNNU3R0UUtnVGtWVV8tRER3RzltZA2oxWFZA2VktnVzhvTTN6ZAWVQdTFNNWRrbGs1XzhocmtDX0NvdU1aMXFLeWR6dXoxTlYxMWl3VWdPUFAtQVU0SVlRcjVWQ0EZD';
+  const accessToken = '';
 
   const response = await fetch(
     `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&access_token=${accessToken}`
