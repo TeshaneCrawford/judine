c16290b0fe56a35de7eaabd6ccec3e49392f3fc3
Teshane Crawford <crawfordteshane@gmail.com>
2024-04-02 13:08:51 -0500

    Refactor StoriesListItem.vue component

--- components/content/StoriesListItem.vue
+++ components/content/StoriesListItem.vue
@@ -5,6 +5,8 @@ import type { Image } from '../../types/image';
 type Story = {
   _path: string;
   title: string;
+  subtitle?: string;
+  info?: string;
   description?: string;
   cover?: Image;
   date?: string;
@@ -31,7 +33,7 @@ defineProps({
     class="group"
   >
     <div
-      class="relative w-full overflow-hidden rounded-lg sm:aspect-[3/2] md:aspect-square lg:aspect-[2/3] dark:bg-zinc-800"
+      class="card rounded-lg sm:aspect-[3/2] md:aspect-square lg:aspect-[2/3] dark:bg-zinc-800"
     >
       <NuxtImg
         :src="story.cover?.src || 'img/placeholder.jpg'"
@@ -43,19 +45,41 @@ defineProps({
         loading="lazy"
         placeholder
       />
+      <div
+        class="card-body absolute rounded-lg w-[100%] h-[100%] top-0 -right-full backdrop-blur-sm bg-white/30 p-8 flex flex-col text-slate-50 justify-center"
+      >
+        <h3>{{ story.title }}</h3>
+        <p
+          v-if="story.subtitle"
+          class="text-sm"
+        >
+          {{ story.subtitle }}
+        </p>
+        <p
+          v-if="story.info"
+          class="text-lg my-10"
+        >
+          {{ story.info }}
+        </p>
+        <p
+          v-if="story.date"
+          class="mt-1 text-sm italic dark:text-zinc-500"
+        >
+          {{ useFormatDate(story.date) }}
+        </p>
+      </div>
     </div>
-    <div
-      class="mt-4 flex items-center justify-between text-base font-medium dark:text-zinc-300"
-    >
-      <h3>{{ story.title }}</h3>
-    </div>
-    <p
-      v-if="story.date"
-      class="mt-1 text-sm italic dark:text-zinc-500"
-    >
-      {{ useFormatDate(story.date) }}
-    </p>
   </NuxtLink>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  @apply relative overflow-hidden;
+}
+.card-body {
+  transition: 2s;
+}
+.card:hover .card-body {
+  @apply right-0;
+}
+</style>
